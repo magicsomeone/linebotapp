105d552d297ba6566858c3042c5387a5d6f10a25
yicheng <magicwtf@gmail.com>
2025-01-02 06:53:46 +0800

    fixed send message.

--- src/controllers/callback.rs
+++ src/controllers/callback.rs
@@ -1,16 +1,42 @@
 #![allow(clippy::missing_errors_doc)]
 #![allow(clippy::unnecessary_struct_initialization)]
 #![allow(clippy::unused_async)]
-use loco_rs::prelude::*;
+
 use axum::debug_handler;
+use line_bot_messaging_api::api::LineApiMessagePushResponse;
+use line_bot_messaging_api::message::{LineApiMessagePushRequest, LineMessageImage, LineMessageText};
+use line_bot_messaging_api::LineClient;
+
+use loco_rs::prelude::*;
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Payload {
+    pub to: Option<String>,
+    pub message: Option<String>,
+}
 
 #[debug_handler]
-pub async fn index(State(_ctx): State<AppContext>) -> Result<Response> {
-    format::empty()
+pub async fn index(
+    State(_ctx): State<AppContext>,
+    Json(_payload): Json<Payload>,
+) -> Result<Response> {
+    let _client_token: String = String::from("/2yGUAc0oJBowI4zIbF0JkykVuXrCKsFGW7VtnK276kQ+dhdtX1A8fgn1pxX/zZflM+Q2fXhMTI8d8wS+7LpwvcQpxTSYG9SeOHaa8roCWhnpmB8ZPxz/V/cy7rFsad7mQeLzjGUuz5iNTE5dxTx6gdB04t89/1O/w1cDnyilFU=");
+
+    let _client = LineClient::new(&_client_token);
+    let _txt_message = LineMessageText::new(&_payload.message.unwrap_or(String::from("empty message")));
+    let _img_message = LineMessageImage::new(original_content_url, preview_image_url);
+    let mut _messages: Vec<Value> = vec![json!(_txt_message)];
+    let _request = LineApiMessagePushRequest {
+        to: String::from(&_payload.to.unwrap_or(String::from("U4bc597171ccc6ec6c99753a7d98e11e3"))),
+        messages: _messages,
+        ..Default::default()
+    };
+    let _response: LineApiMessagePushResponse = _client.message_send_push(&_request).await.unwrap();
+    format::text("ok")
 }
 
 pub fn routes() -> Routes {
-    Routes::new()
-        .prefix("api/callbacks/")
-        .add("/", get(index))
+    Routes::new().prefix("api/callback/").add("/", post(index))
 }
--- src/controllers/linebot.rs
+++ src/controllers/linebot.rs
@@ -6,14 +6,13 @@ use axum::debug_handler;
 use line_bot_messaging_api::api::LineApiMessagePushResponse;
 use line_bot_messaging_api::message::{LineApiMessagePushRequest, LineMessageText};
 use line_bot_messaging_api::LineClient;
-use line_bot_messaging_api::webhook::LineWebhook;
+
 use loco_rs::prelude::*;
 use serde_json::{json, Value};
-use tracing::log;
+
 
 #[debug_handler]
 pub async fn index(State(_ctx): State<AppContext>) -> Result<Response> {
-    // LineClient::web
     let _client = LineClient::new("/2yGUAc0oJBowI4zIbF0JkykVuXrCKsFGW7VtnK276kQ+dhdtX1A8fgn1pxX/zZflM+Q2fXhMTI8d8wS+7LpwvcQpxTSYG9SeOHaa8roCWhnpmB8ZPxz/V/cy7rFsad7mQeLzjGUuz5iNTE5dxTx6gdB04t89/1O/w1cDnyilFU=");
     let _message = LineMessageText::new("Hello, World!");
     let mut _messages: Vec<Value> = vec![json!(_message)];
